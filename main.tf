9262bcb910da7851e2b76feb483164e51bda5671
dnprocks <dnprocks@yahoo.com.br>
2025-04-12 12:34:38 -0300

    change keys to credential file

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region                   = var.region
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "localstack"
+  # skip_credentials_validation = true
+  # skip_metadata_api_check     = true
+  # skip_requesting_account_id  = true
 
   endpoints {
     sts = var.sts_endpoint
