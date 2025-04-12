d61f47f23671313cf400fcfe44f7465545fbbad1
dnprocks <dnprocks@yahoo.com.br>
2025-04-12 12:49:34 -0300

    change outputs to outputs file

---
+++ outputs.tf
@@ -0,0 +1,48 @@
+
+output "instance_public_ip" {
+  value       = aws_instance.vpn.public_ip
+  description = "Endereco IP publico da instancia VPN"
+}
+
+output "instance_ssh_command" {
+  value = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.vpn.public_ip}"
+}
+
+output "instance_private_ip" {
+  value       = aws_instance.vpn.private_ip
+  description = "Endereco IP privado da instancia VPN"
+}
+
+output "instance_id" {
+  value       = aws_instance.vpn.id
+  description = "ID da instancia VPN"
+}
+
+output "instance_ssh_port" {
+  value       = var.ssh_port
+  description = "Porta SSH da instancia VPN"
+}
+
+output "instance_wireguard_port" {
+  value       = var.wireguard_port
+  description = "Porta WireGuard da instancia VPN"
+}
+
+output "instance_security_group_id" {
+  value       = aws_security_group.vpn_sg.id
+  description = "ID do grupo de seguranca da instancia VPN"
+}
+
+output "instance_security_group_tags" {
+  value       = aws_security_group.vpn_sg.tags
+  description = "Tags do grupo de seguranca da instancia VPN"
+}
+
+output "instance_public_dns" {
+  value = aws_instance.vpn.public_dns
+}
+
+output "instance_private_dns" {
+  value = aws_instance.vpn.private_dns
+}
+
